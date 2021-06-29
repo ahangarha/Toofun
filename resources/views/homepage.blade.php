b79f3fda290307cf8fc90d7974f3c16f2c920341
Mostafa Ahangarha <ahangarha@riseup.net>
2021-06-29 23:29:40 +0430

    add links to homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,14 +1,16 @@
 <x-guest-layout>
     <div>
         <div class="grid grid-cols-1 md:grid-cols-2 gap-4 text-center">
-            <div class="border rounded-xl p-8">
+            <a class="border rounded-xl p-8 bg-indigo-100 hover:bg-indigo-200 transform transition hover:-translate-y-1 hover:shadow-lg"
+                href="{{ route('topic-create') }}">
                 <h2 class="text-2xl font-bold">Add New Topic</h2>
                 <h3>and ask for comments</h3>
-            </div>
-            <div class="border rounded-xl p-8">
-                <h2 class="text-2xl font-bold"> Add Comment </h2>
-                <h3>to an existing topic</h3>
-            </div>
+            </a>
+            <a class="border rounded-xl p-8 bg-indigo-100 hover:bg-indigo-200 transform transition hover:-translate-y-1 hover:shadow-lg"
+                href="{{ route('topic-index') }}">
+                <h2 class="text-2xl font-bold"> Check a Topic </h2>
+                <h3>to see/add comments</h3>
+            </a>
         </div>
     </div>
 </x-guest-layout>
