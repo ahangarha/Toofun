465f3839dc4386a1e1b59bf7e1bf7044630e15ea
Mostafa Ahangarha <ahangarha@riseup.net>
2021-06-29 20:58:24 +0430

    add temporary topic index page

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -16,7 +16,7 @@ class TopicsController extends Controller
     public function index()
     {
         //
-        return 'index page';
+        return view('topic.index');
     }
 
     /**
---
+++ resources/views/topic/index.blade.php
@@ -0,0 +1,5 @@
+<x-guest-layout>
+    This page should be made!
+    <br>
+    There will be a form for entering token to get access to an existing topic.
+</x-guest-layout>
